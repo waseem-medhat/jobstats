ce38a9217707c5f9cb2d50bce584438bf487aa3c
wipdev-tech <wipdev.tech@gmail.com>
2024-05-08 13:15:03 +0300

    Add initial web server with default handlers

---
+++ cmd/jobstats/jobstats.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	routerV1 := http.NewServeMux()
+	routerV1.HandleFunc("/ready", handleWelcome)
+	routerV1.HandleFunc("/error", handleError)
+	routerV1.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		respondError(w, http.StatusNotFound, "not found")
+	})
+
+	router := http.NewServeMux()
+	router.Handle("/v1/", http.StripPrefix("/v1", routerV1))
+	router.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		respondError(w, http.StatusNotFound, "not found")
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
+
+func handleWelcome(w http.ResponseWriter, r *http.Request) {
+	welcome := struct {
+		Message string `json:"message"`
+	}{
+		Message: "welcome to the jobstats API",
+	}
+	respondJSON(w, http.StatusOK, welcome)
+}
+
+func handleError(w http.ResponseWriter, r *http.Request) {
+	respondError(w, http.StatusTeapot, "server gives error as expected")
+}
+
+func respondJSON(w http.ResponseWriter, statusCode int, payload any) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(payload)
+}
+
+func respondError(w http.ResponseWriter, statusCode int, errMsg string) {
+	type err struct {
+		Error string `json:"error"`
+	}
+	respondJSON(w, statusCode, err{Error: errMsg})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/waseem-medhat/jobstats
+
+go 1.22.2
